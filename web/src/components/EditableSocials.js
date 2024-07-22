b6da0056ae504ea1804fa7c6de01cdce75937060
Abhay <jayabhay9@gmail.com>
2024-07-23 00:26:06 +0530

    editor logic updates

--- web/src/components/EditableSocials.js
+++ web/src/components/EditableSocials.js
@@ -5,7 +5,7 @@ import { RiPhoneFill } from "react-icons/ri";
 import { AiFillMobile, AiFillHome } from 'react-icons/ai';
 import { GrMail } from 'react-icons/gr';
 import { BsLinkedin } from 'react-icons/bs';
-import { FaGithubSquare } from 'react-icons/fa';
+import { FaGithubSquare, FaLinkedinIn, FaGithub } from 'react-icons/fa';
 import { BiCheck } from 'react-icons/bi';
 import { MdAdd, MdClose } from 'react-icons/md';
 import { FaPhoneSquareAlt } from "react-icons/fa";
@@ -18,8 +18,8 @@ import { fetchDefaultData } from '../utils/resume-structure';
 
 const EditableSocials = (props) => {
     const { tmpID, editorWidth, field, editOn, val, hndlChange } = props;
-    const [editVal, setEditVal] = useState(val);
     const socials_field = fetchDefaultData(tmpID).socials;
+    const [editVal, setEditVal] = useState(val);
 
     const inWidth = (value) => {
         const fntSize = editorWidth * 0.014;
@@ -28,14 +28,6 @@ const EditableSocials = (props) => {
         return { width: inpWidth + 'px' }
     };
 
-    useEffect(() => {
-        setEditVal(val);
-    }, [val]);
-
-    useEffect(() => {
-        console.log("test", editVal);
-        // hndlChange('object', 'socials', editVal);
-    }, [editVal])
 
     const linkIcon = (type) => {
         switch (tmpID) {
@@ -43,8 +35,8 @@ const EditableSocials = (props) => {
                 case 'phone': return <RiPhoneFill className="socials_icon" />
                 case 'email': return <GrMail className="socials_icon" />
                 case 'portfolio': return <AiFillHome className="socials_icon" />
-                case 'linkedin': return <BsLinkedin className="socials_icon" />
-                case 'github': return <FaGithubSquare className="socials_icon" />
+                case 'linkedin': return <FaLinkedinIn className="socials_icon" />
+                case 'github': return <FaGithub className="socials_icon" />
             }
             case '66672b96fe02b86cb6927afa': switch (type) {
                 case 'phone': return <RiPhoneFill className="socials_icon" />
@@ -65,25 +57,25 @@ const EditableSocials = (props) => {
 
     const normalView = () => {
         return <div className='socials_normal'>
-            {editVal && editVal.phone_code && editVal.phone_number && <div className='socials_normal_item'>
+            {val && val.phone_code && val.phone_number && <div className='socials_normal_item'>
                 {val.icons && <div className="socials_icon_root"> {linkIcon("phone")}</div>}
-                <p>{editVal?.phone_code + " " + editVal?.phone_number}</p>
+                <p>{val?.phone_code + " " + val?.phone_number}</p>
             </div>}
-            {editVal && editVal.email && <div className='socials_normal_item'>
+            {val && val.email && <div className='socials_normal_item'>
                 {val.icons && <div className="socials_icon_root"> {linkIcon("email")}</div>}
-                <p>{editVal?.email}</p>
+                <p>{val?.email}</p>
             </div>}
-            {editVal && editVal.portfolio_label && editVal.portfolio_value && <div className='socials_normal_item'>
+            {val && val.portfolio_label && val.portfolio_value && <div className='socials_normal_item'>
                 {val.icons && <div className="socials_icon_root">{linkIcon("portfolio")}</div>}
-                <p>{editVal?.portfolio_label}</p>
+                <p>{val?.portfolio_label}</p>
             </div>}
-            {editVal && editVal.linkedin_label && editVal.linkedin_value && <div className='socials_normal_item'>
+            {val && val.linkedin_label && val.linkedin_value && <div className='socials_normal_item'>
                 {val.icons && <div className="socials_icon_root"> {linkIcon("linkedin")}</div>}
-                <p>{editVal?.linkedin_label}</p>
+                <p>{val?.linkedin_label}</p>
             </div>}
-            {editVal && editVal.github_label && editVal.github_value && <div className='socials_normal_item'>
+            {val && val.github_label && val.github_value && <div className='socials_normal_item'>
                 {val.icons && <div className="socials_icon_root">{linkIcon("github")}</div>}
-                <p>{editVal?.github_label}</p>
+                <p>{val?.github_label}</p>
             </div>}
         </div>
     }
@@ -91,109 +83,81 @@ const EditableSocials = (props) => {
     const editView = () => {
         return <div className='socials_edit_root'>
             <div className='socials_edit_fields'>
-                {editVal.phone_code !== undefined && <div className='socials_edit_item'>
+                {val.phone_code !== undefined && <div className='socials_edit_item'>
                     <span className='edit_label'>phone</span>
                     <select
                         className='edit_select'
-                        style={{ ...inWidth(phone_codes.find(item => item.value === editVal?.phone_code).label) }}
-                        value={editVal?.phone_code}
-                        onChange={(ev) => setEditVal(prevVal => ({
-                            ...prevVal,
-                            phone_code: ev.target.value
-                        }))}
+                        style={{ ...inWidth(phone_codes.find(item => item.value === val?.phone_code).label) }}
+                        value={val?.phone_code}
+                        onChange={(ev) => hndlChange('object', 'socials', { ...val, phone_code: ev.target.value })}
                     >
                         <option value="" />
                         {phone_codes.map(item => (
                             <option value={item.value}>{item.label}</option>
                         ))}
                     </select>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.phone_number) }} type="tel" maxLength={10} value={editVal?.phone_number} onChange={(ev) => {
-                        console.log("sfsfa", ev.target.value);
-                        setEditVal(prevVal => ({
-                            ...prevVal,
-                            phone_number: ev.target.value
-                        }))
+                    <input className='edit_input' style={{ ...inWidth(val?.phone_number) }} type="tel" maxLength={10} value={val?.phone_number} onChange={(ev) => {
+                        hndlChange('object', 'socials', { ...val, phone_number: ev.target.value })
                     }} />
                     <MdClose onClick={() => {
-                        let updated = editVal;
+                        let updated = val;
                         delete updated.phone_code;
                         delete updated.phone_number;
                         setEditVal({ ...updated });
                     }} className='edit_value_close' />
                 </div>}
-                {editVal.email !== undefined && <div className='socials_edit_item'>
+                {val.email !== undefined && <div className='socials_edit_item'>
                     <span className='edit_label'>email</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.email) }} type="email" value={editVal?.email} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        email: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.email) }} type="email" value={val?.email} onChange={(ev) => hndlChange('object', 'socials', { ...val, email: ev.target.value })} />
                     <MdClose onClick={() => {
-                        let updated = editVal;
+                        let updated = val;
                         delete updated.email;
                         setEditVal({ ...updated });
                     }} className='edit_value_close' />
                 </div>}
-                {editVal.portfolio_label !== undefined && editVal.portfolio_value !== undefined && <div className='socials_edit_item'>
+                {val.portfolio_label !== undefined && val.portfolio_value !== undefined && <div className='socials_edit_item'>
                     <span className='edit_label'>portfolio</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.portfolio_label) }} type="text" value={editVal?.portfolio_label} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        portfolio_label: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.portfolio_label) }} type="text" value={val?.portfolio_label} onChange={(ev) => hndlChange('object', 'socials', { ...val, portfolio_label: ev.target.value })} />
                     <span>&#9679;</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.portfolio_value) }} type="text" value={editVal?.portfolio_value} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        portfolio_value: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.portfolio_value) }} type="text" value={val?.portfolio_value} onChange={(ev) => hndlChange('object', 'socials', { ...val, portfolio_value: ev.target.value })} />
                     <MdClose onClick={() => {
-                        let updated = editVal;
+                        let updated = val;
                         delete updated.portfolio_label;
                         delete updated.portfolio_value;
                         setEditVal({ ...updated });
                     }} className='edit_value_close' />
                 </div>}
-                {editVal.linkedin_label && editVal.linkedin_value && <div className='socials_edit_item' >
+                {val.linkedin_label && val.linkedin_value && <div className='socials_edit_item' >
                     <span className='edit_label'>linkedin</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.linkedin_label) }} type="text" value={editVal?.linkedin_label} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        linkedin_label: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.linkedin_label) }} type="text" value={val?.linkedin_label} onChange={(ev) => hndlChange('object', 'socials', { ...val, linkedin_label: ev.target.value })} />
                     <span>&#9679;</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.linkedin_value) }} type="text" value={editVal?.linkedin_value} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        linkedin_value: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.linkedin_value) }} type="text" value={val?.linkedin_value} onChange={(ev) => hndlChange('object', 'socials', { ...val, linkedin_value: ev.target.value })} />
                     <MdClose onClick={() => {
-                        let updated = editVal;
+                        let updated = val;
                         delete updated.linkedin_label;
                         delete updated.linkedin_value;
                         setEditVal({ ...updated });
                     }} className='edit_value_close' />
                 </div>}
-                {editVal.github_label && editVal.github_value && <div className='socials_edit_item'>
+                {val.github_label && val.github_value && <div className='socials_edit_item'>
                     <span className='edit_label'>github</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.github_label) }} type="text" value={editVal?.github_label} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        github_label: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.github_label) }} type="text" value={val?.github_label} onChange={(ev) => hndlChange('object', 'socials', { ...val, github_label: ev.target.value })} />
                     <span>&#9679;</span>
-                    <input className='edit_input' style={{ ...inWidth(editVal?.github_value) }} type="text" value={editVal?.github_value} onChange={(ev) => setEditVal(prevVal => ({
-                        ...prevVal,
-                        github_value: ev.target.value
-                    }))} />
+                    <input className='edit_input' style={{ ...inWidth(val?.github_value) }} type="text" value={val?.github_value} onChange={(ev) => hndlChange('object', 'socials', { ...val, github_value: ev.target.value })} />
                     <MdClose onClick={() => {
-                        let updated = editVal;
+                        let updated = val;
                         delete updated.github_label;
                         delete updated.github_value;
-                        setEditVal({ ...updated });
+                        // setEditVal({ ...updated });
                     }} className='edit_value_close' />
                 </div>}
             </div >
             <div className='socials_edit_add'>
-                {!(editVal.phone_code && editVal.phone_number) && <span className="add_field" onClick={() => { setEditVal({ ...editVal, phone_code: socials_field.phone_code, phone_number: socials_field.phone_number }) }} ><MdAdd className='skills_value_add' />phone</span>}
-                {!editVal.email && <span className="add_field" onClick={() => { setEditVal({ ...editVal, email: socials_field.email }) }} ><MdAdd className='skills_value_add' />email</span>}
-                {!(editVal.portfolio_label && editVal.portfolio_value) && <span className="add_field" onClick={() => { setEditVal({ ...editVal, portfolio_label: socials_field.portfolio_label, portfolio_value: socials_field.portfolio_value }) }}><MdAdd className='skills_value_add' />portfolio</span>}
-                {!(editVal.linkedin_label && editVal.linkedin_value) && <span className="add_field" onClick={() => { setEditVal({ ...editVal, linkedin_label: socials_field.linkedin_label, linkedin_value: socials_field.linkedin_value }) }} ><MdAdd className='skills_value_add' />linkedin</span>}
-                {!(editVal.github_label && editVal.github_value) && <span className="add_field" onClick={() => { setEditVal({ ...editVal, github_label: socials_field.github_label, github_value: socials_field.github_value }); }} ><MdAdd className='skills_value_add' />github</span>}
+                {!(val.phone_code && val.phone_number) && <span className="add_field" onClick={() => { hndlChange('object', 'socials', { ...val, phone_code: socials_field.phone_code, phone_number: socials_field.phone_number }) }} ><MdAdd className='skills_value_add' />phone</span>}
+                {!val.email && <span className="add_field" onClick={() => { hndlChange('object', 'socials', { ...val, email: socials_field.email }) }} ><MdAdd className='skills_value_add' />email</span>}
+                {!(val.portfolio_label && val.portfolio_value) && <span className="add_field" onClick={() => { hndlChange('object', 'socials', { ...val, portfolio_label: socials_field.portfolio_label, portfolio_value: socials_field.portfolio_value }) }}><MdAdd className='skills_value_add' />portfolio</span>}
+                {!(val.linkedin_label && val.linkedin_value) && <span className="add_field" onClick={() => { hndlChange('object', 'socials', { ...val, linkedin_label: socials_field.linkedin_label, linkedin_value: socials_field.linkedin_value }) }} ><MdAdd className='skills_value_add' />linkedin</span>}
+                {!(val.github_label && val.github_value) && <span className="add_field" onClick={() => { hndlChange('object', 'socials', { ...val, github_label: socials_field.github_label, github_value: socials_field.github_value }) }} ><MdAdd className='skills_value_add' />github</span>}
             </div>
         </div >
     }
